97829fc3f66b8426b614ea7a5b2691958786d41b
Edmund Jang <117239582+Edmund-Jang@users.noreply.github.com>
2022-11-22 13:18:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -118,3 +118,76 @@ $(function(){
     });
     
   });
+
+
+
+  function doImgPop(img){
+    img1= new Image();
+    img1.src=(img);
+    imgControll(img);
+   }
+    
+   function imgControll(img){
+    if((img1.width!=0)&&(img1.height!=0)){
+       viewImage(img);
+     }
+     else{
+        controller="imgControll('"+img+"')";
+        intervalID=setTimeout(controller,20);
+     }
+   }
+   
+   function viewImage(img){
+    W=img1.width;
+    H=img1.height;
+    O="width="+W+",height="+H+",scrollbars=yes";
+    imgWin=window.open("","",O);
+    imgWin.document.write("<html><head><title>이미지상세보기</title></head>");
+    imgWin.document.write("<body topmargin=0 leftmargin=0>");
+    imgWin.document.write("<img src="+img+" onclick='self.close()' style='cursor:pointer;' title ='클릭하시면 창이 닫힙니다.'>");
+    imgWin.document.close();
+   }
+
+
+
+
+   let imageIndex = 0;
+   let postion = 0;
+   const IMAGE_WIDTH = 100;
+   const btnPrevious = document.querySelector(".previous")
+   const btnNext = document.querySelector(".next")
+   const smimages = document.querySelector(".smimages")
+
+   function prev(){
+    if(imageIndex > 0) {
+        btnNext.removeAttribute("disabled")
+        postion += IMAGE_WIDTH;
+        smimages.style.transform = `translateX(${postion}%)`;
+        imageIndex = imageIndex -1;
+    }
+    if(imageIndex == 0) {
+        btnPrevious.setAttribute('disabled', 'true')
+    }
+   }
+
+   function next(){
+    if(imageIndex < 5) {
+        btnPrevious.removeAttribute("disabled")
+        postion -= IMAGE_WIDTH;
+        smimages.style.transform = `translateX(${postion}%)`;
+        imageIndex = imageIndex +1;
+    }
+    if(imageIndex == 5) {
+        btnNext.setAttribute('disabled', 'true')
+    }
+
+   }
+
+   function init(){
+    btnPrevious.setAttribute('disabled','true')
+    btnPrevious.addEventListener("click", prev)
+    btnNext.addEventListener("click",next)
+   }
+
+   init();
+   
